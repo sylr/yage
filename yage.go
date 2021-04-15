a72e77a00695fbc5195796dceeb794f103dd1b6a
Sylvain Rabot <sylvain@abstraction.fr>
2021-04-15 14:12:02 +0200

    Improve permissions of output file
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- yage.go
+++ yage.go
@@ -550,12 +550,20 @@ func newLazyOpener(name string, overwrite bool) io.WriteCloser {
 func (l *lazyOpener) Write(p []byte) (n int, err error) {
 	if l.f == nil && l.err == nil {
 		oFlags := os.O_WRONLY | os.O_CREATE
+		perms := os.FileMode(0660)
+
 		if l.overwrite {
+			stat, err := os.Stat(l.name)
+			if err != nil {
+				return 0, err
+			}
+			perms = stat.Mode()
 			oFlags = oFlags | os.O_TRUNC
 		} else {
 			oFlags = oFlags | os.O_EXCL
 		}
-		l.f, l.err = os.OpenFile(l.name, oFlags, 0666)
+
+		l.f, l.err = os.OpenFile(l.name, oFlags, perms)
 	}
 	if l.err != nil {
 		return 0, l.err
